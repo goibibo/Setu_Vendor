6bab18ab1e93d7cd4a379fb83efe953b29277ed8
shilpavenkatesh <shilpa.naik@goibibo.com>
2017-07-07 12:20:17 +0530

    Update v3.py

--- v3.py
+++ v3.py
@@ -4,7 +4,7 @@ import json
 import time
 import logging
 from selenium import webdriver
-import HTMLTestRunner
+#import HTMLTestRunner
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
